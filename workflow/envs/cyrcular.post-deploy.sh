8131c3188af0ba7e00207b22d00c0d25a248a490
Till Hartmann <till.hartmann@udo.edu>
2022-09-27 20:33:36 +0000

    update cyrcular and varlociraptor

--- workflow/envs/cyrcular.post-deploy.sh
+++ workflow/envs/cyrcular.post-deploy.sh
@@ -1,1 +1,1 @@
-cargo install --git https://github.com/tedil/cyrcular.git --branch main --rev ec9fc44682134fad6cc3730eacfe3860cacf6dc3 --locked
+cargo install --git https://github.com/tedil/cyrcular.git --branch annotate --rev 9ddd0ddf9574d05b418e83c70eea687d5228fb1c --locked
