e3f580fcf76059b00d023e5834ec0c3f6b59f7b8
David Laehnemann <david.laehnemann@hhu.de>
2023-04-05 17:23:43 +0200

    also remove --no-track after reading docs

--- workflow/envs/cyrcular.post-deploy.sh
+++ workflow/envs/cyrcular.post-deploy.sh
@@ -1,1 +1,1 @@
-cargo install --git https://github.com/tedil/cyrcular.git --branch annotate --rev 44ba5274b9f2aee7d193b774343acaccf0edec53 --locked --root "${CONDA_PREFIX}" --no-track
+cargo install --git https://github.com/tedil/cyrcular.git --branch annotate --rev 44ba5274b9f2aee7d193b774343acaccf0edec53 --locked --root "${CONDA_PREFIX}"
