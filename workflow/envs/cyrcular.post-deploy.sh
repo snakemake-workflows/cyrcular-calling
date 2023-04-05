b3798592f5bfcbb4b2b27b937cc6f26dc536c37d
David Laehnemann <david.laehnemann@hhu.de>
2023-04-05 15:34:45 +0200

    debug, debug, debug printing

--- workflow/envs/cyrcular.post-deploy.sh
+++ workflow/envs/cyrcular.post-deploy.sh
@@ -1,3 +1,6 @@
-echo "CARGO_HOME: $CARGO_HOME"
-echo "$CARGO_HOME/bin" >> PATH
+echo "PATH: $PATH"
+echo "HOME: $HOME"
+echo "GITHUB_HOME: $GITHUB_HOME"
+echo "GITHUB_PATH: $GITHUB_PATH"
+PATH="$HOME/.cargo/bin:$PATH"
 cargo install --git https://github.com/tedil/cyrcular.git --branch annotate --rev 44ba5274b9f2aee7d193b774343acaccf0edec53 --locked
