bf11c0f400e9a79b75c6c2b97315dca022b75132
David Laehnemann <david.laehnemann@hhu.de>
2023-04-05 16:32:56 +0200

    ruget install PATH cleanup

--- workflow/envs/cyrcular.post-deploy.sh
+++ workflow/envs/cyrcular.post-deploy.sh
@@ -1,6 +1,4 @@
-echo "PATH: $PATH"
-echo "HOME: $HOME"
-echo "GITHUB_HOME: $GITHUB_HOME"
-echo "GITHUB_PATH: $GITHUB_PATH"
+# this is needed to have the `ruget` installed by `plotly_kaleido`
+# available in the PATH, it is removed from PATH after installed
 PATH="$HOME/.cargo/bin:$PATH"
-cargo install --git https://github.com/tedil/cyrcular.git --branch annotate --rev 44ba5274b9f2aee7d193b774343acaccf0edec53 --locked
+PATH=${PATH#[^:]*:}
