e0290c970a8b59d842a635cadbb8f524b3c6916c
David Laehnemann <david.laehnemann@hhu.de>
2023-04-05 16:38:43 +0200

    try if `cargo install --root` also fixes the `ruget` binary location issue

--- workflow/envs/cyrcular.post-deploy.sh
+++ workflow/envs/cyrcular.post-deploy.sh
@@ -1,5 +1,1 @@
-# this is needed to have the `ruget` installed by `plotly_kaleido`
-# available in the PATH, it is removed from PATH after installed
-PATH="$HOME/.cargo/bin:$PATH"
 cargo install --git https://github.com/tedil/cyrcular.git --branch annotate --rev 44ba5274b9f2aee7d193b774343acaccf0edec53 --locked --root "${CONDA_PREFIX}" --no-track
-PATH=${PATH#[^:]*:}
