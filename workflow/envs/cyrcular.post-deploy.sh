ce02a54515747ada7fe06abeb36c776f65e680d4
David Laehnemann <david.laehnemann@hhu.de>
2023-04-05 14:10:03 +0200

    try echoing $CARGO_HOME in cyrcular.post-deploy.sh and append to $PATH

--- workflow/envs/cyrcular.post-deploy.sh
+++ workflow/envs/cyrcular.post-deploy.sh
@@ -1,1 +1,3 @@
+echo "CARGO_HOME: $CARGO_HOME"
+echo "$CARGO_HOME/bin" >> PATH
 cargo install --git https://github.com/tedil/cyrcular.git --branch annotate --rev 44ba5274b9f2aee7d193b774343acaccf0edec53 --locked
