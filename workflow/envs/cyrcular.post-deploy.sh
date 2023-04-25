09c63424578e2c8073122fcecf88ff17ef4ada06
David Laehnemann <david.laehnemann@hhu.de>
2023-04-25 13:23:01 +0200

    add TODO to cyrcular.post-deploy.sh

--- workflow/envs/cyrcular.post-deploy.sh
+++ workflow/envs/cyrcular.post-deploy.sh
@@ -1,5 +1,6 @@
 # this is needed to have the `ruget` binary installed by `plotly_kaleido`
 # available in the PATH, it is removed from PATH after installation of cyrcular
 PATH="$HOME/.cargo/bin:$PATH"
+# TODO: merge branch `annotate` in cyrcular and create bioconda recipe
 cargo install --git https://github.com/tedil/cyrcular.git --branch annotate --rev 44ba5274b9f2aee7d193b774343acaccf0edec53 --locked --root "${CONDA_PREFIX}"
 PATH=${PATH#[^:]*:}
