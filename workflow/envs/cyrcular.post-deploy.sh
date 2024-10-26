7823b501b4a327d30be79c77a5ecc9d3eae1ea29
dlaehnemann <david.laehnemann@hhu.de>
2024-10-26 23:00:08 +0200

    fix: switch to latest commit on cyrcular branch, with fixes included

--- workflow/envs/cyrcular.post-deploy.sh
+++ workflow/envs/cyrcular.post-deploy.sh
@@ -2,5 +2,5 @@
 # available in the PATH, it is removed from PATH after installation of cyrcular
 PATH="$HOME/.cargo/bin:$PATH"
 # TODO: merge branch `annotate` in cyrcular and create bioconda recipe
-cargo install --git https://github.com/tedil/cyrcular.git --branch fix--make-table-command-work-with-any-set-of-event-definitions --rev ac7a6d8d3bdb15a57d621e1d7f588f6ee875ae91 --locked --root "${CONDA_PREFIX}"
+cargo install --git https://github.com/tedil/cyrcular.git --branch fix--make-table-command-work-with-any-set-of-event-definitions --rev e6f18dd287ae9233dbcf6fd7d0b05039512816f8 --locked --root "${CONDA_PREFIX}"
 PATH=${PATH#[^:]*:}
